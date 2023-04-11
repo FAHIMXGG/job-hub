d2971eca811d54f52dac7e54bc9414d1f6885585
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-04-11 15:12:32 +0600

    see all jobs function added

--- src/components/JobCard/JobCard.jsx
+++ src/components/JobCard/JobCard.jsx
@@ -6,13 +6,23 @@ import Job from '../Job/Job';
 const JobCard = () => {
 
     const [jobsData, setJobsData] = useState([]);
+    const [visible, setVisible] = useState(4);
+
+    const showMoreItems = () =>{
+        setVisible((prevValue) => prevValue + 2);
+    };
+
     useEffect(() => {
         fetch('JobData.json')
             .then(res => res.json())
             .then(data => setJobsData(data))
     }, [])
 
-    
+    //console.log(visible)
+    let button;
+    if(visible === 4){
+        button = <button style={ {background:' linear-gradient(90deg, #7E90FE 0%, #9873FF 100%)'}} className='mt-5 mb-16 text-center mx-auto flex justify-center px-5 py-2 rounded text-white' onClick={showMoreItems}>See All Jobs</button>
+    }
 
     return (
         <div className='p-5 md:p-0'>
@@ -24,7 +34,7 @@ const JobCard = () => {
             <div className='md:mx-60 '>
                 <div className='grid lg:grid-cols-2 justify-items-center gap-5'>
                     {
-                        jobsData.map(jobData => <JobCardInfo
+                        jobsData.slice(0, visible).map(jobData => <JobCardInfo
                             key={jobData._id}
                             jobData={jobData}
                             
@@ -33,6 +43,8 @@ const JobCard = () => {
                     
                 </div>
             </div>
+            {button}
+            
         </div>
     );
 };
