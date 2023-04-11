7422298deaabbe128e00e58ea420fe13fbdf0f43
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-04-11 22:21:52 +0600

    404text

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -11,6 +11,7 @@ export default function ErrorPage() {
 
     return (
         <div id="error-page" className='text-center mt-32'>
+            <h1 className='font-bold mb-2 text-3xl'>404</h1>
             <h1 className='font-bold mb-2 text-3xl'>Oops!Oops!</h1>
             
             
